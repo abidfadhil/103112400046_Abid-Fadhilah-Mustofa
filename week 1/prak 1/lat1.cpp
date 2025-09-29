821a179b02e793ec8e9a967df655dc6f7b6bd9f8
abidfadhil <abidataufadhil@gmail.com>
2025-09-29 21:24:14 +0700

    Delete week 1 directory

--- week 1/prak 1/lat1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){ 
-int umur;
- cout << " masukan umur anda : ";
- cin >> umur;
- cout << " umur anda adalah " << umur << " tahun. " << end;
- return 0;
-}
